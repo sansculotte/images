dcad1a2a163c2ded701d26a1a99aaa28262f82bc
Ulrich Berthold <ub@artfacts.net>
2019-09-19 12:10:06 +0200

    serve images from directory

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
-
+use std::fs::File;
 use rocket::{Request, response::content, data::Data};
 
 
+const IMAGE_DIR: &str = "images/";
+
+
 #[get("/<domain>/<id>")]
-fn index(domain: String, id: String) -> String {
-    format!("Artfacts image server: {} {}", domain, id)
+fn images(domain: String, id: String) -> Option<content::Plain<File>> {
+    let filename = format!(
+        "{path}/{domain}/{id}.jpg",
+        path=IMAGE_DIR,
+        domain=domain,
+        id=id
+    );
+    File::open(&filename).map(|f| content::Plain(f)).ok()
 }
 
 #[catch(404)]
@@ -22,7 +32,7 @@ fn not_found(request: &Request) -> content::Html<String> {
 
 fn main() {
     rocket::ignite()
-        .mount("/", routes![index])
+        .mount("/", routes![images])
         .register(catchers![not_found])
         .launch();
 }
