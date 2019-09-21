fffdadcd6e80e33c39d4c6e27b2d3eeb6a2cbddf
Ulrich Berthold <u@sansculotte.net>
2019-09-21 15:49:49 +0200

    scale and crop for fixed proportion thumbnail

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@
 
 #[macro_use]
 extern crate rocket;
+extern crate image;
 
 use std::fs::File;
+use image::{FilterType, PNG, JPEG};
 use rocket::{Request, response::content, data::Data};
 
 
@@ -21,6 +23,43 @@ fn images(domain: String, id: String) -> Option<content::Plain<File>> {
     File::open(&filename).map(|f| content::Plain(f)).ok()
 }
 
+#[get("/<domain>/thumb/<id>")]
+fn scaled(domain: String, id: String)
+    -> Option<content::Plain<File>>
+{
+    let format = "thumb";
+    let cacheFilename = format!(
+        "{path}/{domain}/{format}/{id}.jpg",
+        path=IMAGE_DIR,
+        domain=domain,
+        format=format,
+        id=id
+    );
+    let f = File::open(&cacheFilename);
+    match f {
+        Ok(file) => Some(content::Plain(file)),
+        Err(error) => {
+            let filename = format!(
+                "{path}/{domain}/{id}.jpg",
+                path=IMAGE_DIR,
+                domain=domain,
+                id=id
+            );
+            let i = image::open(filename);
+            match i {
+                Ok(img) => {
+                    let scaled = img.resize_to_fill(280, 180, FilterType::Nearest);
+                    let mut output = File::create(cacheFilename).unwrap();
+                    scaled.write_to(&mut output, JPEG).unwrap();
+                    output.sync_all();
+                    Some(content::Plain(output))
+                },
+                Err(error) => None 
+            }
+        }
+    }
+}
+
 #[catch(404)]
 fn not_found(request: &Request) -> content::Html<String> {
     let html = format!(
@@ -32,7 +71,7 @@ fn not_found(request: &Request) -> content::Html<String> {
 
 fn main() {
     rocket::ignite()
-        .mount("/", routes![images])
+        .mount("/", routes![images, scaled])
         .register(catchers![not_found])
         .launch();
 }
