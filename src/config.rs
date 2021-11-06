dd9967e739b948c51748f506d8be01151bbce32d
Ulrich Berthold <u@sansculotte.net>
2021-11-06 23:00:37 +0100

    configure image root directory, thumb dimensions and cache level with rocket config, pass via State to endpoints

---
+++ src/config.rs
@@ -0,0 +1,48 @@
+const IMAGE_DIR: &str = "images/";
+const THUMB_WIDTH: i64 = 280;
+const THUMB_HEIGHT: i64 = 120;
+const CACHE_LEVEL: &str = "Long";
+
+#[derive(Clone, Debug)]
+pub enum CacheLevel {
+    None,
+    Short,
+    Medium,
+    Long
+}
+
+impl CacheLevel {
+    fn parse(s: &str) -> Self {
+        match s {
+            "None" => Self::None,
+            "Short" => Self::Short,
+            "Medium" => Self::Medium,
+            "Long" => Self::Long,
+            _ => Self::Long 
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Config {
+    pub image_dir: String,
+    pub thumb_width: u32,
+    pub thumb_height: u32,
+    pub cache_level: CacheLevel,
+}
+
+impl Config {
+    pub fn from(rocket_config: &rocket::Config) -> Self {
+        let image_dir: String = String::from(rocket_config.get_str("image_dir").unwrap_or(IMAGE_DIR));
+        let thumb_width: u32 = rocket_config.get_int("thumb_width").unwrap_or(THUMB_WIDTH) as u32;
+        let thumb_height: u32 = rocket_config.get_int("thumb_height").unwrap_or(THUMB_HEIGHT) as u32;
+        let cache_level =
+            CacheLevel::parse(rocket_config.get_str("cache_level").unwrap_or(CACHE_LEVEL));
+        Self {
+            image_dir: image_dir,
+            thumb_width: thumb_width,
+            thumb_height: thumb_height,
+            cache_level: cache_level,
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,9 @@
 extern crate image;
+extern crate rocket;
 
-use std::env;
 use std::path::PathBuf;
 use self::image::{FilterType};
 
-
-const IMAGE_DIR: &str = "images/";
-
-fn image_dir() -> String {
-        env::var("IMAGE_DIR").unwrap_or(String::from(IMAGE_DIR))
-}
 
 pub fn resize_and_crop_to(filename: &PathBuf, cached: &PathBuf, width: u32, height: u32) -> Option<()> {
     let i = image::open(filename);
@@ -22,12 +16,12 @@ pub fn resize_and_crop_to(filename: &PathBuf, cached: &PathBuf, width: u32, heig
     }
 }
 
-pub fn get_filename(domain: String, name: String) -> PathBuf {
-    let filename: PathBuf = [ image_dir(), domain, name ].iter().collect();
+pub fn get_filename(image_dir: String, domain: String, name: String) -> PathBuf {
+    let filename: PathBuf = [ image_dir, domain, name ].iter().collect();
     filename
 }
 
-pub fn get_cache_filename(domain: String, name: String, format: String) -> PathBuf {
-    let filename = [ image_dir(), domain, format, name ].iter().collect();
+pub fn get_cache_filename(image_dir: String, domain: String, name: String, format: String) -> PathBuf {
+    let filename = [ image_dir, domain, format, name ].iter().collect();
     filename
 }
